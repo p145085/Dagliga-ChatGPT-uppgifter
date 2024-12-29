6cf43d36bc1eac29e023327c5fc7893599199419
Emil Johansson <emil.a.johansson@gmail.com>
2024-12-29 16:23:36 +0100

    Sort by Length

---
+++ 23/SortLength.py
@@ -0,0 +1,10 @@
+#23. **Sortera strängar efter längd:** Bygg ett program som sorterar en lista av strängar baserat på deras längd.
+
+# Lista med strängar
+strings = ["äpple", "banan", "kiwi", "ananas", "apelsin"]
+
+# Sortera listan efter längden på strängarna
+sorted_strings = sorted(strings, key=len)
+
+# Skriv ut den sorterade listan
+print(sorted_strings)
